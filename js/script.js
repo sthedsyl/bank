5098e95aa9a295b47dbb4e5b52980f117ec6bbb3
Sthefane <sthefanedias04@gmail.com>
2025-05-02 16:21:10 -0300

    criação do script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+
+const camposDoFormualrio = document.querySelectorAll("[required]");
+
+camposDoFormualrio.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+})
+
+function verificaCampo(campo) {
+    if (campo.name=="cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+    if (campo.name =="aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+}
